a32dd8de274caebb3414b486b52d130a68242c03
Artie <temasad12@gmail.com>
2023-10-31 20:00:27 +0200

    Add project files.

---
+++ WebApplicationExample2.Services/MailService.cs
@@ -0,0 +1,48 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+namespace lab2.Services
+{
+    public interface IMailService
+    {
+        void SendMail(string to, string message);
+    }
+
+    public class MailService : IMailService
+    {
+        readonly IConfiguration _configuration;
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void SendMail(string to, string message)
+        {
+            var mailStuff = _configuration.GetSection("Mail");
+            var emailFrom = mailStuff["Username"];
+            var password = mailStuff["Password"];
+
+            var mailHost = mailStuff["MailHost"];
+            var mailPort = int.Parse(mailStuff["MailPort"] ?? "25");
+
+            using var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("sender", emailFrom));
+            emailMessage.To.Add(new MailboxAddress("", to));
+            emailMessage.Subject = "lab work";
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+            using (var client = new SmtpClient())
+            {
+                client.Connect(mailHost, mailPort, SecureSocketOptions.StartTls);
+                client.Authenticate(emailFrom, password);
+                client.Send(emailMessage);
+
+                client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ WebApplicationExample2/Program.cs
@@ -0,0 +1,28 @@
+using lab2.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IMailService, MailService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
